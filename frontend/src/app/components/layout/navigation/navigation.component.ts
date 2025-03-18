19a8fac7d670867e62268357bf07b60b9d2a9e2e
Jakub Smilowski <jakub.smilowski@protonmail.com>
2025-03-18 19:03:01 +0100

    videos now work with moc backend api

--- frontend/src/app/components/layout/navigation/navigation.component.ts
+++ frontend/src/app/components/layout/navigation/navigation.component.ts
@@ -14,6 +14,5 @@ export class NavigationComponent {
     { path: '/security', label: 'Security' },
     { path: '/faq', label: 'FAQ' },
     { path: '/videos', label: 'Videos' },
-    {path: '/autoTag', label: 'autoTag'}
   ];
 }
--- frontend/src/app/models/videos.ts
+++ frontend/src/app/models/videos.ts
@@ -2,7 +2,8 @@ export class Video {
     id: number = 0;
     title: string = "";
     description: string = "";
-    // tags: string[] = ['',''];
-    // image_url: string = "";
+    duration: number = 0;
+    image_url: string = "";
+    tags: string[] = ['',''];
     // video_url: string = "";
 }
