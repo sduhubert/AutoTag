f8232a77939dab449b4f4f81c8852123812bc1f3
Jakub Smilowski <jakub.smilowski@protonmail.com>
2025-04-29 17:40:24 +0200

    uploads folder connected to frontend

--- database/init.sql
+++ database/init.sql
@@ -70,9 +70,9 @@ INSERT INTO "admin" ("email", "username", "password") VALUES
 ('admin@gmail.com', 'admin', 'admin1');
 
 INSERT INTO "video" ("userid", "title", "filepath", "duration") VALUES
-(1, '5 Amazing Facts About Vivaldi ｜ History Brought To Life', 'assets/5 Amazing Facts About Vivaldi ｜ History Brought To Life.mp4', '00:01:46'),
-(2, 'Learn Italian with Luca!', 'assets/[Pixar] Learn Italian with Luca! Pixar.mov', '00:02:47'),
-(1, 'Meet Vivaldi ｜ Composer Biography for Kids.mp3', 'assets/Meet Vivaldi ｜ Composer Biography for Kids.mp3', '00:06:32');
+(1, '5 Amazing Facts About Vivaldi ｜ History Brought To Life', 'uploads/5 Amazing Facts About Vivaldi ｜ History Brought To Life.mp4', '00:01:46'),
+(2, 'Learn Italian with Luca!', 'uploads/[Pixar] Learn Italian with Luca! Pixar.mov', '00:02:47'),
+(1, 'Meet Vivaldi ｜ Composer Biography for Kids.mp3', 'uploads/Meet Vivaldi ｜ Composer Biography for Kids.mp3', '00:06:32');
 
 INSERT INTO "tag" ("name") VALUES 
 ('Education'),
