f4c0b4516db6cb45c0fd64fe9e73e1bbccb296f9
Tomasz Kareta <jakub.smilowski@protonmail.com>
2025-06-01 20:39:17 +0200

    comments to the code

--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -13,13 +13,13 @@ import {
 
 const router = express.Router();
 
-router.post('/upload', uploadSingle, uploadFile);
-router.get('/', getVideos);
-router.get('/:id', getVideoById);
-router.delete('/:id', deleteVideo);
-router.put('/:id', updateVideo);
-router.put('/:id/tags', updateVideoTags);
-router.put('/:id/summary', updateVideoSummary);
-router.put('/:id/validation', updateVideoValidation);
+router.post('/upload', uploadSingle, uploadFile); //post /api/video/upload
+router.get('/', getVideos); //get /api/video/
+router.get('/:id', getVideoById); // get /api/video/:id
+router.delete('/:id', deleteVideo); // delete /api/video/:id
+router.put('/:id', updateVideo); // put /api/video/:id
+router.put('/:id/tags', updateVideoTags); // PUT /api/video/:id/tags
+router.put('/:id/summary', updateVideoSummary); // PUT  /api/video/:id/summary
+router.put('/:id/validation', updateVideoValidation); //DELETE /api/:id/validation
 
 export default router;
