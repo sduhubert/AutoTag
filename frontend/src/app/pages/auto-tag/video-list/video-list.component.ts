757a948128924411e500bdc9ced8e603bf41dcd5
Tomasz Kareta <jakub.smilowski@protonmail.com>
2025-06-01 15:47:57 +0200

    now audio and video files are supported

--- frontend/src/app/pages/auto-tag/video-list/video-list.component.ts
+++ frontend/src/app/pages/auto-tag/video-list/video-list.component.ts
@@ -49,6 +49,7 @@ export class VideoListComponent implements OnInit{
       this.selectedFile = fileList[0];
       this.message = `File selected: ${this.selectedFile.name}`;
       this.isError = false;
+      console.log(this.selectedFile.name);
     }
   }
 
@@ -60,12 +61,12 @@ export class VideoListComponent implements OnInit{
     }
 
     // Check file type
-    const fileType = this.selectedFile.type;
-    if (!fileType.includes('video/')) {
-      this.message = 'Please select a video file';
-      this.isError = true;
-      return;
-    }
+    // const fileType = this.selectedFile.type;
+    // if (!(fileType.includes('video/') && (fileType.includes('audio/')))) {
+    //   this.message = 'Please select a video file';
+    //   this.isError = true;
+    //   return;
+    // }
 
     this.isUploading = true;
     this.message = '';
@@ -90,6 +91,7 @@ export class VideoListComponent implements OnInit{
           
           // You could add logic here to refresh the videos list
           // or add the newly uploaded video to the list
+          
         },
         error: (error) => {
           this.isUploading = false;
