b029de69b49eca1b784a13b86d235968c818d9a4
sduhubert <husyl23@student.sdu.dk>
2025-06-02 14:47:39 +0200

    remove videoplayer from tabs

--- frontend/src/app/components/layout/navigation/navigation.component.ts
+++ frontend/src/app/components/layout/navigation/navigation.component.ts
@@ -14,7 +14,6 @@ export class NavigationComponent {
     { path: '/security', label: 'Security' },
     { path: '/faq', label: 'FAQ' },
     { path: '/videos', label: 'Videos' },
-    { path: '/videoplayer', label: 'Videoplayer' },
     { path: '/admin', label: 'Admin Dashboard' }
   ];
 }
